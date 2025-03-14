a2c9c157e57dbea28f0c6e786dbb4444f9fb7da9
PWangR <124851875+PWangR@users.noreply.github.com>
2025-03-14 20:59:43 +0700

    BT_Dashboard
    Tiếp tục làm bài

---
+++ BT_Dashboard/app/src/main/java/dp/wang/ClothingActivity.java
@@ -0,0 +1,19 @@
+package dp.wang;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ClothingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing);
+
+    }
+}
