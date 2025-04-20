3c11e934a624383cacf2c404536df0aa79286c6c
PWangR <124851875+PWangR@users.noreply.github.com>
2025-04-20 21:42:49 +0700

    EX_Replacing_Fragments_Dynamically
    Hoàn thành

---
+++ EX_Replacing_Fragments_Dynamically/app/src/main/java/dp/wang/FooterFragment.java
@@ -0,0 +1,62 @@
+package dp.wang;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FooterFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_footer, container, false);
+
+        Button btn1 = v.findViewById(R.id.button);
+        Button btn2 = v.findViewById(R.id.button2);
+        Button btn3 = v.findViewById(R.id.button3);
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView,new Fragment1())
+                        .commit();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView,new Fragment2())
+                        .commit();
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView,new Fragment3())
+                        .commit();
+            }
+        });
+
+        return v;
+    }
+}
